294643d72253d7c36730352fd404bf27b2d0d485
Mark Zuschlag <markazuschlag@gmail.com>
2018-09-15 13:39:36 -0700

    Content is now centered, added CSS and HTML files

--- src/view/View.elm
+++ src/view/View.elm
@@ -3,11 +3,12 @@ module View exposing (view)
 import Model exposing (Model)
 import Update exposing (Msg(..))
 import Html exposing (Html, div, h1, button, text)
+import Html.Attributes exposing (id)
 import Html.Events exposing (onClick)
 
 view : Model -> Html Msg
 view model = 
-    div []
+    div [ id "view" ]
         [ button [ onClick English ] [ text "English" ] 
         , h1 [] [ text model ]
         , button [ onClick Japanese ] [ text "Japanese" ]
