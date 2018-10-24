ef358c3d38c427a9e505397268514c8529182ac4
Mark Zuschlag <markazuschlag@gmail.com>
2018-10-24 00:06:25 -0700

    Added route handling for 404

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const path = require('path');
 
 app.use(express.static("public"));
 
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 app.listen(process.env.PORT || 3000);
