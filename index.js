ea3113ce3f763e065a3f93acb4ea40a912ec1d6e
Mark Zuschlag <markazuschlag@gmail.com>
2018-10-27 17:57:11 -0700

    More deployment issues

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 app.listen(process.env.PORT || 3000);
